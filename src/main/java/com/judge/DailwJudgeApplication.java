3c0f4c78e80e2d00f283e60bf76db5e2c7125ca5
dailiwei <traveler__wei@outlook.com>
2026-03-13 09:50:55 +0800

    Initial commit

---
+++ src/main/java/com/judge/DailwJudgeApplication.java
@@ -0,0 +1,13 @@
+package com.judge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailwJudgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailwJudgeApplication.class, args);
+    }
+
+}
